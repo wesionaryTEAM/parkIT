3d1f01373a5a57c90cd6b4b694369647e5e2fe93
aashishdahal1 <aashdahal@gmail.com>
2020-01-23 22:46:19 +0545

    adds firebase config

---
+++ src/components/fireabse/fireabse/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBfQz155G_Mtkxep2PIDzh0ScvONXJ092M",
+    authDomain: "park-it-a32d4.firebaseapp.com",
+    databaseURL: "https://park-it-a32d4.firebaseio.com",
+    projectId: "park-it-a32d4",
+    storageBucket: "park-it-a32d4.appspot.com",
+    messagingSenderId: "561114454756",
+    appId: "1:561114454756:web:db5f3676082fc5c2ddeaf1",
+    measurementId: "G-VD0P66STK0"
+  };
