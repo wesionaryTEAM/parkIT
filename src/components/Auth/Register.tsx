7a3184c762a0cbc7e2382721be70bebff2d987a0
Aiska Basnet <aiska.basnet@gmail.com>
2020-01-27 22:07:10 +0545

    Changed Login and Register page

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import logo from '../../assests/images/logo.png';
-import { Paper } from '@material-ui/core';
+import { Paper, Box } from '@material-ui/core';
 import { ButtonComponent } from '../Layouts/ButtonComponent';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { userDataProps } from '../../interface/UserInterface'
+import validator from "validator";
+import axios from 'axios'
+import backgroundImage from '../../assests/images/carpark.png'
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -20,7 +21,7 @@ const useStyles = makeStyles(theme => ({
     paper: {
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
+        textAlign: 'left',
         padding: theme.spacing(6),
     },
     form: {
@@ -35,104 +36,263 @@ const useStyles = makeStyles(theme => ({
     },
     paperText: {
         color: "#41a886"
+    },
+    customError: {
+        color: 'red',
+        fontSize: '0.8rem',
+        marginTop: '10px'
+    },
+    box: {
+        background: "url(" + backgroundImage + ")",
+        padding: theme.spacing(10)
+    },
+    title: {
+        color: "#fff",
+
+    },
+    label: {
+        color: '#636161'
+    },
+    textRight: {
+        textAlign: 'right',
+
+    },
+    textColor: {
+        color: '#545454'
+    },
+    link: {
+        fontSize: '15px',
+        textDecoration: 'none',
+        color: '#545454',
+        "&:hover": {
+            color: "#4e5bb9"
+        }
+    },
+    image: {
+        width: '80%'
     }
 }));
 
 
-function Register() {
+interface profileData extends userDataProps {
+    firstname: string;
+    lastname: string;
+    password: string;
+    confirmPassword: string;
+}
+
+interface FormErrors extends profileData {
+    errors: string;
+  }
+
+function Register(props: userDataProps) {
+
     const classes = useStyles();
+    const history = useHistory();
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+        firstname: "",
+        lastname: "",
+        confirmPassword: ""
+    } as profileData);
+    const [errors, setErrors] = useState({} as FormErrors);
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        setLoading(true);
+        
+        clearErrors();
+        if (!validateForm()) {
+            setLoading(false);
+            return;
+        }
+
+        const userData = {
+            email: values.email,
+            password: values.password,
+            firstname: values.firstname,
+            lastname: values.lastname
+        }
+        // TODO firebase method call 
+    }
+
+    const clearErrors = () => {
+        const errors: { [key: string]: string } = {};
+        errors.email="";
+        errors.password = "";
+        handleErrors(errors);
+    }
+    const handleChange = (e: any) => {
+        e.persist();
+        setValues(values => ({
+            ...values,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const validateForm = () => {
+        const errors: { [key: string]: string } = {};
+
+        if (!validator.trim(values.email)) {
+            errors.email = "Email field is required!";
+        }
+
+        if (!validator.trim(values.password)) {
+            errors.password = "Password field is required!";
+        }
+
+        if (!validator.trim(values.confirmPassword)) {
+            errors.confirmPassword = "Password field is required!";
+        }
+
+        if(!validator.trim(values.firstname)) {
+            errors.firstname = "Firstname is required!";
+        }
+
+        if(!validator.trim(values.lastname)) {
+            errors.lastname = "Lastname is required!";
+        }
+
+        if(values.password !== values.confirmPassword){
+            errors.confirmPassword = "Password not matched!";
+        }
+
+        handleErrors(errors);
+
+        return Object.keys(errors).length === 0;
+    };
+
+    const handleErrors = (newErrors: { [key: string]: string }) => {
+        setErrors(errors => ({
+            ...errors,
+            ...newErrors
+        }));
+    };
     return (
-        <Container
-            component="main"
-            className={classes.container}
-            maxWidth="md">
-            <CssBaseline />
-            <Grid
-                container
-                alignContent="center"
-                alignItems="center"
-                justify="center"
-                className={classes.loginCard}
-                spacing={6}>
+        <Box>
+            <Box className={classes.box}>
+                <Typography variant="h4" className={classes.title}>
+                    <Box fontWeight={600} letterSpacing={3}>
+                        REGISTER
+                    </Box>
+                </Typography>
+            </Box>
+            <Container
+                component="main"
+                className={classes.container}
+                maxWidth="md">
+                <CssBaseline />
                 <Grid
-                    item
-                    md={4}>
-                    <img src={logo} alt="Parkit Logo" />
+                    container
+                    alignContent="center"
+                    alignItems="center"
+                    justify="center"
+                    className={classes.loginCard}
+                    spacing={3}>
+                    <Grid
+                        item
+                        md={3}>
+                        <img className={classes.image} src={logo} alt="Parkit Logo" />
+                    </Grid>
+                    <Grid item md={9}>
+                        <Paper className={classes.paper}>
+                            <Box className={classes.form}>
+                                <Grid container spacing={1}>
+                                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                                        <TextField
+                                            variant="outlined"
+                                            margin="normal"
+                                            value={values.firstname}
+                                            fullWidth
+                                            id="firstname"
+                                            label="First Name*"
+                                            name="firstname"
+                                            type="text"
+                                            onChange={handleChange}
+                                            helperText={errors.firstname}
+                                            error={errors.firstname ? true : false}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={12} md={6} lg={6}>
+                                        <TextField
+                                            variant="outlined"
+                                            margin="normal"
+                                            value={values.lastname}
+                                            fullWidth
+                                            id="lastname"
+                                            label="Last Name*"
+                                            name="lastname"
+                                            type="text"
+                                            onChange={handleChange}
+                                            helperText={errors.lastname}
+                                            error={errors.lastname ? true : false}
+                                        />
+                                    </Grid>
+                                    </Grid>
+                                        <TextField
+                                            variant="outlined"
+                                            margin="normal"
+                                            value={values.email}
+                                            fullWidth
+                                            id="email"
+                                            label="Email*"
+                                            name="email"
+                                            type="email"
+                                            onChange={handleChange}
+                                            helperText={errors.email}
+                                            error={errors.email ? true : false}
+                                        />
+                                        <TextField
+                                            variant="outlined"
+                                            margin="normal"
+                                            value={values.password}
+                                            fullWidth
+                                            id="password"
+                                            label="Password*"
+                                            name="password"
+                                            type="password"
+                                            onChange={handleChange}
+                                            helperText={errors.password}
+                                            error={errors.password ? true : false}
+                                        />
 
-                </Grid>
-                <Grid
-                    item
-                    md={7}>
-                    <Paper className={classes.paper}>
-                        <Typography className={classes.paperText} variant="h5">
-                            Sign Up
-                    </Typography>
-                        <form className={classes.form} noValidate>
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-
-                                label="Full Name"
-                                name="full_name"
-                                autoComplete="full_name"
-                                autoFocus
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-
-
-                            />
-
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="confirmPassword"
-                                label="Confirm Password"
-                                type="confirmPassword"
-
-
-                            />
-
-                            <ButtonComponent
-                                primary
-                                size="large"
-                                type="submit">
-                                Sign In
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    value={values.confirmPassword}
+                                    fullWidth
+                                    name="confirmPassword"
+                                    label="Confirm Password*"
+                                    type="password"
+                                    onChange={handleChange}
+                                    helperText={errors.confirmPassword}
+                                    error={errors.confirmPassword ? true : false}
+                                />
+                                <ButtonComponent
+                                    primary
+                                    size="large"
+                                    type="submit"
+                                    onClick={handleSubmit}
+                                    disabled={loading}
+                                    loading={loading}>
+                                        Register
                                 </ButtonComponent>
-                            <Grid container>
-
-                                <Grid item >
-                                    <Link to="/">
-                                        {"Already have account? Sign In"}
+                                <Typography variant="h6" color="textSecondary" align="center">
+                                    OR
+                                </Typography>
+                                <Typography align="center">
+                                    <Link to="/" className={classes.link}>
+                                        {"Already have an account? Sign in"}
                                     </Link>
-                                </Grid>
-                            </Grid>
-                        </form>
-                    </Paper>
+                                </Typography>
+                            </Box>
+                        </Paper>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Container >
+            </Container >
+        </Box>
     )
 }
 export default Register;
